1b740277099249c24e6cd4ded7f3707e4906209f
vika41130 <vika41130@gmail.com>
2026-05-28 12:39:49 +0700

    portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:blink/app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const BlinkApp());
 }
